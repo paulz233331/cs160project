c43ce32981814f6f8be5dd16839de194dffa71ae
Paul Zheng <paul.zheng@sjsu.edu>
2021-06-15 10:01:51 -0700

    Update test.spec.js

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -1,14 +1,26 @@
+//jest.useFakeTimers();
+
+var app = require('./../app');
+//app.main();
+
 describe("Testing with Jest", () => {
-  test("Addition", () => {
+//setTimeout(app.main(), 10000);
+    //await app.main();
+/*
+    beforeAll(() => {
+        app.main();
+    });
+*/
+  test("Resume field", async() => {
+    await app.main();
     const sum = 2 + 3;
     const expectedResult = 5;
     expect(sum).toEqual(expectedResult);
   });
-    
-  // Jest also allows a test to run multiple
-  // times using different values
-  test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 6]])(
+
+/*  test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 6]])(
   'Does %i + %i equals %i', (a, b, expectedResult) => {
     expect(a + b).toBe(expectedResult);
-  });
+  }); */
 });
+
