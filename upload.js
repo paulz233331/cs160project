f465bc4fe5ee9870ad8c9941b964bffe1600cb78
Paul Zheng <paul.zheng@sjsu.edu>
2021-06-15 13:46:24 -0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,18 +1,24 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+const path = require('path');
+var app = require('./app');
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path;
-      var newpath = '\public/' + files.filetoupload.name;
+      var newpath = '\public/' + files.filetoupload.name.split('.').slice(0, -1).join('.') +
+       "Upload" + path.parse(files.filetoupload.name).ext;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
         res.end();
-      });
+    });
+
+    app.main();
+
  });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
