93d769d53eba9f06f060858b8d1cbde34848678f
Paul Zheng <paul.zheng@sjsu.edu>
2021-07-15 11:54:54 -0700

    Update Employer.jsx

---
+++ client/src/pages/employer/Employer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './employer.css';
+
+function Employer() {
+    return (
+        <html>
+        <head>
+            <script type="text/javascript">
+        </script>
+        </head>
+        <body topmargin="40" leftmargin="40">
+             This is the employer's page.
+             
+         </body>
+        </html>
+    )
+}
+
+export default Employer
