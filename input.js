ad857aa939b5c947d1f1a81779d767d54e1abfa7
Paul Zheng <paul.zheng@sjsu.edu>
2021-07-15 20:40:06 -0700

    Update input.js

--- input.js
+++ input.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+
 //https://attacomsian.com/blog/uploading-files-nodejs-express
 expr.use(fileUpload({
     createParentPath: true
@@ -732,9 +733,11 @@ expr.post('/test', function(req, res) {
                         <head>
                             <script type="text/javascript">
                             </script>
+                            <style>
+                            </style>
                         </head>
                         <body topmargin="40" leftmargin="40">
-                            You are redirected here after submitting text input.
+                        <h1>Applicant Dashboard</h1>
         `;
         //html = html + JSON.stringify(result);
         html = html +
